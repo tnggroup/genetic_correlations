e48769442408862db4b85be92e94628d734cfc96
Johan Källberg Zvrskovec <johan.akz@gmail.com>
2023-07-28 11:44:27 +0100

    package updates to troubleshoot the gsheet read issues.

--- R/standardCleanAndMunge.R
+++ R/standardCleanAndMunge.R
@@ -225,7 +225,9 @@ standardPipelineCleanAndMunge <- function(
         #iAltPath<-1
         cFilepath <- as.character(file.path(altInputFolderPaths[iAltPath],sumstats_meta[iTrait,c("file_name")]))
         if( !is.na(sumstats_meta[iTrait,]$file_name) & file.exists(cFilepath)){
+          print(cFilepath)
           sumstats_meta[iTrait,path_orig:=eval(cFilepath)]
+          print(sumstats_meta[iTrait,c("path_orig")])
           break
         }
 
